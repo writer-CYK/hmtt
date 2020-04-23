a7ee29436174f1d2228e6f61466cdc738dc72308
CaoYukun <17602686210@163.com>
2020-04-23 11:16:13 +0800

    初始化项目目录2

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,16 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 不去检测 function 后面是否有空格
+    'space-before-function-paren': 0,
+    // 不去检测代码后面是否有分号
+    semi: 0
   }
-}
+};
